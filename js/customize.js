73face83928413f6d0cc94e18e12b1ed990c40ff
Tseng <tseng7510@gmail.com>
2023-07-13 18:02:26 +0800

    更新搜尋功能

--- js/customize.js
+++ js/customize.js
@@ -372,38 +372,46 @@ const videoSlider = new Swiper('.videoSlider .swiper', {
 /*-----------------------------------*/
 /////////////modal設定/////////////
 /*-----------------------------------*/
-$(function () {
-  $('#modal1').hide(); //先隱藏視窗
-  $('.modal').after('<div class="modal_overlay"></div>'); //新增透明底
-  $('.modal').prepend('<button type="button" class="close">關閉</button>'); //新增關閉按鈕
-  $('.modal_overlay').hide(); //隱藏透明底
-  //按鈕動作
-  $('#openModal').click(function (e) {
-    $('.modal_overlay').fadeIn(100);
-    $('.modal').fadeIn(100);
-    $('body').addClass('noscroll');
-    e.preventDefault();
-  });
-  //關閉function
-  function closeModal() {
-    $('#modal1').hide();
-    $('.modal_overlay').hide();
-    $('body').removeClass('noscroll');
-  }
-  //點選關閉按鈕及透明底都可關閉
-  $('.modal_overlay').click(closeModal);
-  $('.modal .close').click(closeModal);
-});
+// $(function () {
+//   $('#modal1').hide(); //先隱藏視窗
+//   $('.modal').after('<div class="modal_overlay"></div>'); //新增透明底
+//   $('.modal').prepend('<button type="button" class="close">關閉</button>'); //新增關閉按鈕
+//   $('.modal_overlay').hide(); //隱藏透明底
+//   //按鈕動作
+//   $('#openModal').click(function (e) {
+//     $('.modal_overlay').fadeIn(100);
+//     $('.modal').fadeIn(100);
+//     $('body').addClass('noscroll');
+//     e.preventDefault();
+//   });
+//   //關閉function
+//   function closeModal() {
+//     $('#modal1').hide();
+//     $('.modal_overlay').hide();
+//     $('body').removeClass('noscroll');
+//   }
+//   //點選關閉按鈕及透明底都可關閉
+//   $('.modal_overlay').click(closeModal);
+//   $('.modal .close').click(closeModal);
+// });
 
 //pageSearch
-function pageSearch() {
-  var x = document.getElementById('pageSearchBox');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
-    x.style.display = 'none';
-  }
-}
+// function pageSearch() {
+//   var x = document.getElementById('pageSearchBox');
+//   console.log(x.style.display);
+//   if (x.style.display === 'none') {
+//     x.style.display = 'block';
+//   } else {
+//     x.style.display = 'none';
+//   }
+// }
+(function () {
+  const pageSearchBtn = document.querySelector('.pageSearchBtn');
+  const pageSearchBox = document.querySelector('#pageSearchBox');
+  pageSearchBtn?.addEventListener('click', () => {
+    window.getComputedStyle(pageSearchBox).display === 'none' ? (pageSearchBox.style.display = 'block') : (pageSearchBox.style.display = 'none');
+  });
+})();
 
 //people
 $(function () {
